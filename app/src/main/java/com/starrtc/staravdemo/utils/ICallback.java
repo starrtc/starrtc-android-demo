b1327f25d273dd1fb063a2b8784f5f13e046e9b8
www.elesos.com <895377235@qq.com>
2018-06-21 17:18:57 +0800

    fix bug

--- app/src/main/java/com/starrtc/staravdemo/utils/ICallback.java
+++
@@ -1,5 +0,0 @@
-package com.starrtc.staravdemo.utils;
-
-public interface ICallback {
-    abstract  void callback(boolean reqSuccess, String statusCode, String data);
-}
--- app/src/main/java/com/starrtc/staravdemo/utils/IEventListener.java
+++
@@ -1,5 +0,0 @@
-package com.starrtc.staravdemo.utils;
-
-public interface IEventListener {
-    abstract public void dispatchEvent(String aEventID, boolean success, Object eventObj);
-}
